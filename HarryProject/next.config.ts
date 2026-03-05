05e221a76c01b76680f1e8a83d8aefdc020d13e7
Harshada Mali <harshadamali2003@gmail.com>
2026-03-05 19:37:17 +0530

    fix next static export

--- HarryProject/next.config.ts
+++ HarryProject/next.config.ts
@@ -1,13 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "http",
         hostname: "localhost",
         port: "5000",
-        // Allow all image paths from the backend (e.g. /tmp/uploads/..., /uploads/...)
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "sweethome-backend-70n7.onrender.com",
         pathname: "/**",
       },
     ],
